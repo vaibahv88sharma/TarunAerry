00ca6c2fc319c9eb32b69a3c6d1569dcad082fdd
vaibhav <vaibhav88sharma@gmail.com>
2016-10-10 15:17:32 +1100

    new

---
+++ scripts/MyScripts/services/taxationService.js
@@ -0,0 +1,60 @@
+﻿angular.module('homeModule').factory('taxationService', ["$http", "$q", "$resource", function ($http, $q, $resource) {
+
+
+    function getAllTaxrResource(id) {
+        if (id) {
+            return $resource(
+                "/scripts/MyScripts/sourceTaxation.json",
+                { id: id },
+                {
+                    query: {
+                        method: 'GET',
+                    }
+                }
+            );
+        }
+        else {
+            return $resource(
+                "/scripts/MyScripts/sourceTaxation.json",
+                {},
+                {
+                    query: {
+                        method: 'GET',
+                    }
+                }
+            );
+        }
+    }
+
+    function getAllResource() {
+        var resource = getAllTaxrResource();
+        var deferred = $q.defer();
+
+        resource.query({},
+            function (data) {
+                deferred.resolve(data);
+            }, function (error) {
+                deferred.reject(error);
+            });
+        return deferred.promise;
+    }
+
+    function getSelTaxResource(id) {
+        var resource = getAllTaxrResource(id);
+        var deferred = $q.defer();
+
+        resource.query({},
+            function (data) {
+                deferred.resolve(data);
+            }, function (error) {
+                deferred.reject(error);
+            });
+        return deferred.promise;
+    }
+
+    return {
+        getAllResource: getAllResource,
+        getSelTaxResource: getSelTaxResource
+    };
+
+}]);
