9c89646174a841019a5bff92072248c10ced2ee7
vaibhav <vaibhav88sharma@gmail.com>
2016-10-11 12:57:30 +1100

    NavPage

---
+++ scripts/MyScripts/directives/navPage.js
@@ -0,0 +1,20 @@
+﻿angular.module('homeModule').directive('navPage', function () {
+    return {
+        replace: true,
+        restrict: "E",
+        //scope:false,
+        //scope: {
+        //    image: "=",
+        //    title: "=",
+        //    description: "=",
+        //},
+        //require: "?ngModel",
+        templateUrl: "template/directive/navPage.html",
+        controller: function ($scope, $location) {
+            $scope.isPage = function (name) {
+                return new RegExp("/" + name + "($|/)").test($location.path());
+            };
+        }
+
+    };
+});
